6d9b06a5bbcaeab99e1ace7afd8022026626a657
mayankmalviya015 <mayankmalviya2620@gmail.com>
2026-02-18 00:16:10 +0530

    add python files

---
+++ basic/gradeSystem.py
@@ -0,0 +1,13 @@
+marks =int(input("enter your marks: "))
+if(marks>=90):
+    print("A+")
+elif(marks<90 and marks>=80):
+    print("A")
+elif(marks <80 and marks >=60):
+    print("B")
+elif(marks<60 and marks>=40):
+    print("C")
+elif(marks<40 and marks>=33):
+    print("D")
+else:
+    print("you are fail")                    
---
+++ basic/trafficSystem.py
@@ -0,0 +1,7 @@
+Light_name =input("name : ")
+if(Light_name=="yellow"):
+    print("ready for go")
+elif(Light_name=="red"):
+    print("stop")
+else : 
+    print("goo")        
